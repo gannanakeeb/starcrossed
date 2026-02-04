ee43c12beca91668ec4f6ac95a008b276fd13ef9
todarima330 <ooomrfawzy@gmail.com>
2026-02-04 16:03:51 +0200

    meow

--- scripts/riddle_area.gd
+++ scripts/riddle_area.gd
@@ -2,23 +2,13 @@ extends Area2D
 
 
 @export var required_stars := 3
-@export var riddle_ui: Control
-func _ready():
-	riddle_ui.hide()
-	print("Riddle UI reference:", riddle_ui)
+@export var riddle_ui: CanvasLayer
 
-
-
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(body):
 	if not body.is_in_group("player"):
-		return   
+		return
 
-	print("👤 Player entered riddle area")
-
-	print("Riddle Area check! Player has: ", body.stars_collected)
-		
 	if body.stars_collected >= required_stars:
-		riddle_ui.show_riddle()
+		riddle_ui.show()
 	else:
 		print("You need more stars")
-		
--- scripts/star.gd
+++ scripts/star.gd
@@ -1,9 +1,8 @@
 extends Area2D
 
 @export var value := 1
-func _on_body_entered(body: Node2D) -> void:
 
+func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.add_star(value)
-		print("⭐ Star collected")
 		queue_free()

975abceec8b629b5b671c698cbfdb937e7cf03bf
gannanakeeb <gannanakeeb@gmail.com>
2026-02-14 15:40:03 +0200

    ,akeljjlwres
    a;ejfp

--- scripts/riddle_area.gd
+++ scripts/riddle_area.gd
@@ -3,12 +3,20 @@ extends Area2D
 
 @export var required_stars := 3
 @export var riddle_ui: CanvasLayer
-
+@onready var my_sprite = $Sprite2D
 func _on_body_entered(body):
 	if not body.is_in_group("player"):
 		return
 
 	if body.stars_collected >= required_stars:
-		riddle_ui.show()
+		my_sprite.visible = true
 	else:
 		print("You need more stars")
+		
+
+func _ready():
+	my_sprite.visible = false
+	
+	
+	
+	
